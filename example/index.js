c07b984a6ca1c9d81bfce0cb27eae90771b026ef
ishibashi_y <ishibashi_y@worksap.co.jp>
2018-12-20 16:00:51 +0900

    feat: Info and Servers object

--- example/index.js
+++ example/index.js
@@ -1,8 +1,19 @@
-const { Swagger } = require('../dist');
+const swagger = require('../dist');
 
-class MyAPI extends Swagger {
+class MyAPI extends swagger.Swagger {
   constructor () {
     super();
+
+    new swagger.Info(this, {
+      title: 'Swagger Petstore',
+      version: '1.0.0',
+    });
+
+    new swagger.Servers(this, [
+      {
+        url: 'http://petstore.swagger.io/v1'
+      }
+    ]);
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,41 @@
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 
+export interface InfoProps {
+  title: string,
+  description?: string,
+  version?: string,
+};
+
+export class Info {
+  constructor (parent: Swagger, props: InfoProps) {
+    parent._addObject('info', props);
+  }
+}
+
+export interface ServersProps {
+  url: string,
+}
+
+export class Servers {
+  constructor (parent: Swagger, props: Array<ServersProps>) {
+    parent._addObject('servers', props);
+  }
+}
+
 export class Swagger {
   outfile: string = 'openapi.yml';
+  object: any = {};
 
-  constructor () {
+  constructor (openapi?: string) {
+    this._addObject('openapi', '3.0.0');
+  }
+
+  _addObject (key: string, value: any) {
+    this.object[key] = value;
   }
 
   run () {
-    fs.writeFileSync(this.outfile, yaml.dump({
-      'openapi': '3.0.0',
-    }));
+    fs.writeFileSync(this.outfile, yaml.dump(this.object));
   }
 }
