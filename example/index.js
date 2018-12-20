5ecef0e834997e7fbf45a1a0755650379920297a
ishibashi_y <ishibashi_y@worksap.co.jp>
2018-12-20 17:20:19 +0900

    feat: Path object

--- example/index.js
+++ example/index.js
@@ -1,22 +1,53 @@
 const swagger = require('../dist');
 
-class MyAPI extends swagger.Swagger {
-  constructor () {
-    super();
+const api = new swagger.Swagger();
 
-    new swagger.Info(this, {
-      title: 'Swagger Petstore',
-      version: '1.0.0',
-    });
+api.addInfo({
+  title: 'Swagger Petstore',
+  version: '1.0.0',
+});
 
-    new swagger.Servers(this, [
-      {
-        url: 'http://petstore.swagger.io/v1'
-      }
-    ]);
+api.addServers([
+  {
+    url: 'http://petstore.swagger.io/v1',
   }
-}
+]);
 
-const api = new MyAPI();
+api.addPath(
+  '/pets',
+  swagger.HttpMethod.GET,
+  new swagger.Path({
+    summary: 'List all pets',
+    operationId: 'listPets',
+    tags: [
+      'pets',
+    ],
+  })
+  .addParameter({
+    name: 'limit',
+    in: 'query',
+    description: 'How many items to return at one time (max 100)',
+    required: false,
+    schema: {
+      type: 'integer',
+      format: 'int32',
+    },
+  })
+  .addResponse(
+    '200',
+    new swagger.Response({
+      description: 'A paged array of pets'
+    })
+    .addHeader('x-next', {
+      description: 'A link to the next page of responses',
+      schema: {
+        _ref: '#/components/schemas/Pets',
+      }
+    })
+    .addContent('application/json', {
+      _ref: '#/components/schemas/Error',
+    })
+  )
+);
 
 api.run();
--- src/index.ts
+++ src/index.ts
@@ -5,37 +5,167 @@ export interface InfoProps {
   title: string,
   description?: string,
   version?: string,
-};
-
-export class Info {
-  constructor (parent: Swagger, props: InfoProps) {
-    parent._addObject('info', props);
-  }
 }
 
 export interface ServersProps {
   url: string,
 }
 
-export class Servers {
-  constructor (parent: Swagger, props: Array<ServersProps>) {
-    parent._addObject('servers', props);
+export enum HttpMethod {
+  POST = <any>"post",
+  GET = <any>"get",
+  PUT = <any>"put",
+  DELETE = <any>"delete",
+  PATCH = <any>"patch",
+  OPTIONS = <any>"options",
+}
+
+type SchemaProps = {
+  type: string,
+  items: SchemaProps,
+} | {
+  type: string,
+  properties: Map<string, SchemaProps>,
+} | {
+  _ref: string,
+} | {
+  type: string,
+  format?: string,
+};
+
+export interface ParameterProps {
+  name: string,
+  in: string,
+  description: string,
+  required: boolean,
+  schema: SchemaProps,
+}
+
+export interface PathProps {
+  summary: string,
+  operationId: string,
+  tags: Array<string>,
+}
+
+export interface HeaderProps {
+  description: string,
+  schema: SchemaProps,
+}
+
+export interface ResponseProps {
+  description: string,
+}
+
+function mapToObj<T>(arg: Map<string, T>, func: (arg0: T) => object = (x : any) => x): object {
+  const obj: any = {};
+  arg.forEach((value, key) => {
+    obj[key] = func(value);
+  });
+
+  return obj;
+};
+
+export class Response {
+  props: ResponseProps;
+  headers: Map<string, HeaderProps> = new Map();
+  content: Map<string, SchemaProps> = new Map();
+
+  constructor (props: ResponseProps) {
+    this.props = props;
+  }
+
+  addHeader (header: string, props: HeaderProps): Response {
+    this.headers.set(header, props);
+    return this;
+  }
+
+  addContent (contentType: string, schema: SchemaProps): Response {
+    this.content.set(contentType, schema);
+    return this;
+  }
+
+  render (): object {
+    return Object.assign(
+      this.props,
+      {
+        headers: mapToObj(this.headers),
+        content: mapToObj(this.content),
+      }
+    )
+  }
+}
+
+export class Path {
+  props: PathProps;
+  parameters: Array<ParameterProps> = [];
+  responses: Map<string, Response> = new Map();
+
+  constructor (props: PathProps) {
+    this.props = props;
+  }
+
+  addParameter (props: ParameterProps): Path {
+    this.parameters.push(props);
+    return this;
+  }
+
+  addResponse (statusCode: string, object: Response): Path {
+    this.responses.set(statusCode, object);
+    return this;
+  }
+
+  render (): object {
+    return Object.assign(
+      this.props,
+      {
+        parameters: this.parameters,
+        responses: mapToObj(this.responses, r => r.render()),
+      }
+    );
   }
 }
 
 export class Swagger {
   outfile: string = 'openapi.yml';
   object: any = {};
+  paths: Map<string, Map<HttpMethod, Path>> = new Map();
 
-  constructor (openapi?: string) {
-    this._addObject('openapi', '3.0.0');
+  constructor (openapi: string = '3.0.0') {
+    this.addObject('openapi', openapi);
+    this.addObject('paths', {});
   }
 
-  _addObject (key: string, value: any) {
+  private addObject (key: string, value: any) {
     this.object[key] = value;
   }
 
+  addInfo(props: InfoProps) {
+    this.addObject('info', props);
+  }
+
+  addServers(props: ServersProps) {
+    this.addObject('servers', props);
+  }
+
+  addPath(path: string, method: HttpMethod, object: Path) {
+    if (!this.paths.has(path)) {
+      this.paths.set(path, new Map());
+    }
+
+    this.paths.get(path).set(method, object);
+  }
+
   run () {
+    let pathObject: any = {};
+    this.paths.forEach((methods, path) => {
+      pathObject[path] = {};
+
+      methods.forEach((object, method) => {
+        pathObject[path][method] = object.render();
+      });
+    });
+    this.object['paths'] = pathObject;
+
     fs.writeFileSync(this.outfile, yaml.dump(this.object));
   }
 }
