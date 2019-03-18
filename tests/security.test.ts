0444d50e3c5473144bc2b9b73ddfb7fbc1abc911
funasaka_s <funasaka_s@worksap.co.jp>
2019-03-18 12:16:29 +0900

    refactor: security test

--- tests/security.test.ts
+++ tests/security.test.ts
@@ -4,7 +4,7 @@ describe('Operation Object', () => {
   it('should add paths object with requestBody and responses', () => {
     const swagger = new devkit.Swagger();
 
-    swagger.addSecurityComponent({
+    const securityComponent = {
       BasicAuth: {
         type: 'http',
         scheme: 'basic',
@@ -27,36 +27,16 @@ describe('Operation Object', () => {
           }
         }
       }
-    })
+    }
+
+
+    swagger.addSecurityComponent(securityComponent)
 
     const expected  = {
       components: {
         schemas: {},
-        securitySchemes: {
-          BasicAuth: {
-            type: 'http',
-            scheme: 'basic'
-          },
-          BearerAuth: {
-            type: 'http',
-            scheme: 'bearer'
-          },
-          OAuth2: {
-            type: 'oauth2',
-            flow: {
-              authorizationCode: {
-                authorizationUrl: 'https://example.com/oauth/authorize',
-                tokenUrl: 'https://example.com/oauth/token',
-                scopes: {
-                  read: 'Grants read access',
-                  write: 'Grants write access',
-                  admin: 'Grants access to admin operations'
-                }
-              }
-            }
-          }
-        }
-      }
+        securitySchemes: securityComponent,
+      },
     };
 
 
